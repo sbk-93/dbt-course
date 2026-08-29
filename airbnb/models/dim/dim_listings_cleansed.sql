e7d5000be9cb595b83fe8ac8886a75f9b09aa8cc
Balakrishnan Subramaniam <bala.subramaniam@sjp.co.uk>
2026-08-29 21:09:53 +0100

    Materialization completed

--- airbnb/models/dim/dim_listings_cleansed.sql
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -1,3 +1,7 @@
+{{config(
+    materialized = 'view')
+    }}
+
 WITH src_listings AS (
   SELECT
     *
@@ -13,7 +17,8 @@ SELECT
     ELSE minimum_nights
   END AS minimum_nights,
   host_id,
-  REPLACE(price_str,'$') :: NUMBER(10,2) AS price,
+  REPLACE(price_str,'$') :: NUMBER(10,2
+  ) AS price,
   created_at,
   updated_at
 FROM
